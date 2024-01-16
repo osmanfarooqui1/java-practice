04daab88d3fa2d2515c971ed4fd4b102795b9a59
Osman <osmanshakeel77@gmail.com>
2024-01-16 01:05:54 +0000

    used scanner to take input

---
+++ variables_exercise.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class variables_exercise {
+    public static void main(String[]args){
+        Scanner sa = new Scanner(System.in);
+        int a = sa.nextInt();
+        int b = sa.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
