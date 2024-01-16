8e2ba50dae9fe4e98f63bfb687997d423e628f1d
Osman <osmanshakeel77@gmail.com>
2024-01-16 01:01:54 +0000

    add next line output

---
+++ firstclass.java
@@ -0,0 +1,8 @@
+class firstclass{
+    public static void main(String args[]){
+ 
+    System.out.println("Hello World");
+    System.out.print("Hello World\n From Osman\n");
+    System.out.print("Hello World");
+    }
+}
