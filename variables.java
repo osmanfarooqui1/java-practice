47fe9204a13fd7f1a0da2fe131d2a9cf003a9397
Osman <osmanshakeel77@gmail.com>
2024-01-16 01:02:33 +0000

    varibles added

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    
+    public static void main(String[] args){
+    int a= 10;
+    int b=25;
+    int sum= a+b;
+    //System.out.println(sum);
+    int mul = a*b;
+    System.out.println(mul);
+    }
+}
