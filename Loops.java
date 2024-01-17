ab17bac8ab5c0e1435a2f405c61291e7c859a7e2
Osman <osmanshakeel77@gmail.com>
2024-01-17 00:36:14 +0000

    For, while and do while loops basics

---
+++ Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for(int i=0;i<10;i= i+1){
+        //     System.out.println("Hello World");
+        // }
+        int i =12;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i= i+1;
+        // }
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<11);
+    }
+}
