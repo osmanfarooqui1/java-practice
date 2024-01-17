810ff2231c54ab5e6a86c62b3cc7f93857f366e6
Osman <osmanshakeel77@gmail.com>
2024-01-17 01:31:43 +0000

    basic exercise for loop

---
+++ for_loop_exc1.java
@@ -0,0 +1,13 @@
+public class for_loop_exc1 {
+    public static void main(String[] args){
+        // Print the sum of first n natural numbers
+        int n = 4;
+        int sum = 0;
+        for(int i=0;i<=n;i++){
+           sum = sum+i;
+        }
+        System.out.println(sum);
+
+    }
+    
+}
