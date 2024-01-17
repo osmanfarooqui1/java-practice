8b70107400c9c9998f31525e380708374c8e6f64
Osman <osmanshakeel77@gmail.com>
2024-01-17 01:30:56 +0000

    Basics of loop

--- Loops.java
+++ Loops.java
@@ -3,14 +3,15 @@ public class Loops {
         // for(int i=0;i<10;i= i+1){
         //     System.out.println("Hello World");
         // }
-        int i =12;
+        // int i =12;
         // while (i<11) {
         //     System.out.println(i);
         //     i= i+1;
-        // }
-        do{
-            System.out.println(i);
-            i=i+1;
-        }while(i<11);
+        // } 
+        // do while loop prints atleast one time. does the work first then checks the condition
+        // do{
+        //     System.out.println(i);
+        //     i=i+1;
+        // }while(i<11);
     }
 }
